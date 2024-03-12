98580b18ed278dd090a4f6de4d0f109324d59043
Yuval nadam <Yuval.nadam@post.runi.ac.il>
2024-03-12 16:31:29 +0200

    firsttry

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -14,16 +14,29 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i = 0; i < 3000 ; i++ ) {
+
+			dictionary[i] = in.readLine();
+			
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+
+		for (int i = 0; i < dictionary.length; i++ ) {
+
+			if(dictionary[i].equals(word))
+				return true;
+		}
+
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
+
+		hashtag = hashtag.toLowerCase();
 
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
@@ -31,8 +44,18 @@ public class HashTagTokenizer {
         }
  
         int N = hashtag.length();
+      //  int index = 0; // marks the begiging of each word
 
         for (int i = 1; i <= N; i++) {
+
+        	if (existInDictionary(hashtag.substring(0, i), dictionary)) {
+
+        		System.out.println(hashtag.substring(0, i));
+        		breakHashTag(hashtag.substring(i), dictionary);
+
+        	}
+
+
 		
         }
     }
--- SpellChecker.java
+++ SpellChecker.java
@@ -3,6 +3,11 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
+
+		String word1 = "hello";
+		String word2 = "HILLo";
+		System.out.println(levenshtein(word1, word2));
+
 		String word = args[0];
 		int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
@@ -11,11 +16,41 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+
+		return str.substring(1);
+
 	}
 
+	//The function accepts two strings as input and returns the edit distance between these two words, as an integer (known as "levenshtein distance function")
+
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+
+		int count1 = 0;
+		int count2 = 0;
+		int count = 0;
+
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+
+		if(word1.length() == 0)
+			return word2.length();
+
+		if (word2.length() == 0) 
+			return word1.length();
+
+		if (word1.charAt(0) == word2.charAt(0)) 
+
+				count = levenshtein(tail(word1), tail(word2));
+
+			else // 1+min
+		{
+
+			 count1 = 1 + levenshtein(tail(word1), word2);
+			 count2 = 1 + levenshtein(word1, tail(word2));
+        	count = Math.min(Math.min(count1, count2), 1 + levenshtein(tail(word1), tail(word2)));
+		}
+
+		return count;
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -23,13 +58,30 @@ public class SpellChecker {
 
 		In in = new In(fileName);
 
-		// Your code here
+
+		for (int i = 0; i < 3000 ; i++ ) {
+
+			dictionary[i] = in.readLine();
+			
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+
+		String simliarWord = word;
+
+		for (int i = 0; i < dictionary.length; i++ ) {
+
+			if (levenshtein(dictionary[i], word) < threshold)
+				simliarWord = dictionary[i];
+
+			
+		}
+
+		return simliarWord;
+
 	}
 
 }
